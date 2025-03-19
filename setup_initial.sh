63a73344e7f769c4cd671b96df1bc6e1645bda25
Gururaja Hebbar <gururaja.hebbar@netradyne.com>
2025-03-19 17:30:00 +0530

    print important details while setting up qcs repo

--- setup_initial.sh
+++ setup_initial.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "add remote QCS Repo"
 git remote add qcs_repo git@github.com:gururajah-nd/git_clone_testing.git
+
+echo "fetch required branch"
 git fetch qcs_repo qcs_1.3
 
+echo "checkout requires qcs file"
 git checkout qcs_repo/qcs_1.3 setup-environment layers/meta-virtualization/
+
+echo "remove unecessary qcs files from git index"
 git rm --cached -r layers/meta-virtualization/ setup-environment
 
